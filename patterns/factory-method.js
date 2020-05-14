0a71137e1c595c926d5d8b8b8f3f6c8cd4faf75b
Евгений Ишмуратов <ishmuratov@reg.ru>
2020-05-14 09:25:57 +0400

    added factory-method pattern

---
+++ patterns/factory-method.js
@@ -0,0 +1,23 @@
+class TestFactory {
+
+    constructor( amount, balance, comment ) {
+        this.amount = amount;
+        this.balance = balance;
+        this.comment = comment,
+        this.date = '2020-01-09 13:14:58';
+    }
+}
+
+class TestFactoryMethod {
+	create( type ) {
+		if ( type === 'testOne' )
+			return new TestFactory( 108000, 300, 'Оплата за услуги по счёту' );
+		if (type === 'testTwo')
+			return new TestFactory( 111000, 320, 'Списание средств' );
+	}
+}
+
+const factory = new TestFactoryMethod();
+
+const testTypeOne = factory.create('testOne');
+const testTypeTwo = factory.create('testTwo');
