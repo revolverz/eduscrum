8e98c5ea50f2765beb08744bf13191b3520a0612
revolver <43179700+revolverz@users.noreply.github.com>
2020-05-15 09:58:50 +0400

    Update singleton.js

--- patterns/singleton.js
+++ patterns/singleton.js
@@ -1,20 +1,19 @@
 class Counter {
-
-	constructor() {
-		if ( typeof Counter.instance === 'object' ) {
-			return Counter.instance;
-		}
-		this.count = 0;
-		Counter.instance = this;
-		return this;
+    constructor() {
+        if ( typeof Counter.instance === 'object' ) {
+	    return Counter.instance;
+        }
+	    this.count = 0;
+	    Counter.instance = this;
+	    return this;
 	}
 
 	getCount() {
-		return this.count;
+	    return this.count;
 	}
 
 	increaseCount() {
-		return this.count++;
+	    return this.count++;
 	}
 }
 
