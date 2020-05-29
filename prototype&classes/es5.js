09e9213435184e7bbccda06ab4198b279bf6bdba
Евгений Ишмуратов <ishmuratov@reg.ru>
2020-05-29 10:04:54 +0400

    comparison of prototypes and classes

---
+++ prototype&classes/es5.js
@@ -0,0 +1,75 @@
+// es5
+
+function Task(title) {
+    this._title = title;
+    this._done = false;
+    Task.count +=1;
+}
+
+Object.defineProperty(Task, 'title', {
+    get: function() {
+        return this._title;
+    },
+    set: function(value) {
+        this._title = value;
+    }
+});
+
+Task.prototype.complete = function() {
+    this._done = true;
+};
+
+Task.getDefaultTitle = function() {
+    return 'Задача';
+};
+
+Task.count = 0;
+
+function Subtask(title,parent) {
+    Task.call(this,title);
+    this._parent = parent;
+}
+
+SubTask.prototype = Object.create(Task.prototype);
+SubTask.prototype.constructor = SubTask;
+
+var task = new Task('Изучить JS');
+var subtask = new SubTask('Изучить ES6', task);
+
+// es6
+
+class Task {
+    constructor(title = 'Задача') {
+        this._title = title;
+        this._done = false;
+        Task.count +=1;
+    }
+
+    get title() {
+        return this.title;
+    }
+
+    set title(value) {
+        this._title = value;
+    }
+
+    complete() {
+        this._done = true;
+    }
+
+    static getDefaultTitle() {
+        return 'Задача';
+    }
+}
+
+Task.count = 0;
+
+class SubTask extends Task {
+    constructor(title,parent) {
+        super(title);
+        this._parent = parent;
+    }
+}
+
+let task = new Task('Изучить JS');
+let subtask = new SubTask('Изучить ES6', task);
