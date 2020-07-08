cbe8771249e23d947632c379b9e7a708446cb6d3
revolver <43179700+revolverz@users.noreply.github.com>
2020-07-08 12:41:10 +0400

    Update bridge.js

--- patterns/bridge.js
+++ patterns/bridge.js
@@ -1,46 +1,46 @@
 class Model {
-	constructor(color) {
-		this.color = color;
-	}
+    constructor(color) {
+        this.color = color;
+    }
 };
 
 class Color {
-	constructor(type) {
-		this.type = type;
-	}
-	get() {
-		return this.type;
-	}
+    constructor(type) {
+	this.type = type;
+    }
+    get() {
+	return this.type;
+    }
 };
 
 class BlackColor extends Color {
-	constructor() {
-		super("dark-black");
-	}
+    constructor() {
+	super("dark-black");
+    }
 }
 
 class SilbrigColor extends Color {
-	constructor() {
-		super("Silbermetallic");
-	}
+    constructor() {
+	super("Silbermetallic");
+    }
 }
 
 class Audi extends Model {
-	constructor(color) {
-		super(color);
-	}
+    constructor(color) {
+	super(color);
+    }
 
-	paint() {
-		return `Auto: Audi, Color: ${this.color.get()}`;
-	}
+    paint() {
+	return `Auto: Audi, Color: ${this.color.get()}`;
+    }
 };
 
 class Bmw extends Model {
-	constructor(color) {
-		super(color);
-	}
+    constructor(color) {
+	super(color);
+    }
 
-	paint() {
-		return `Auto: Bmw, Color: ${this.color.get()}`;
-	}
+    paint() {
+	return `Auto: Bmw, Color: ${this.color.get()}`;
+    }
 };
